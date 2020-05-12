e18affac90f90d2de2c3a6db9803966e9105840c
amlsakr <amlsakr9@gmail.com>
2020-05-12 18:10:25 +0200

    jandle error

--- app/src/main/java/com/example/covid_19_tracker/network/networkDocumentation/FirstRetrofitClientInterface.kt
+++ app/src/main/java/com/example/covid_19_tracker/network/networkDocumentation/FirstRetrofitClientInterface.kt
@@ -1,7 +1,7 @@
 package com.example.covid_19_tracker.network
 
 
-import com.example.covid_19_tracker.detailsCountry.model.DetailsCountry
+//import com.example.covid_19_tracker.detailsCountry.model.DetailsCountry
 import okio.ByteString
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,8 +9,8 @@ import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface FirstRetrofitClientInterface {
-
-    @Headers("x-rapidapi-host:coronavirus-monitor.p.rapidapi.com","X-RapidAPI-Key:f6199fba62msh899398b08097b40p1bbfb8jsn386c09ded695")
-    @GET("coronavirus/{countryName}")
-    fun getDetailsCountry(@Path("countryName" ) countryName : String) : Call<DetailsCountry>
+//
+//    @Headers("x-rapidapi-host:coronavirus-monitor.p.rapidapi.com","X-RapidAPI-Key:f6199fba62msh899398b08097b40p1bbfb8jsn386c09ded695")
+//    @GET("coronavirus/{countryName}")
+//    fun getDetailsCountry(@Path("countryName" ) countryName : String) : Call<DetailsCountry>
 }
